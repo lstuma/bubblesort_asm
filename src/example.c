8ebd56704307409a70b76dd8c538e6b5f9ad41e3
sdoxl <g.lstuma@gmail.com>
2023-03-20 20:43:19 +0100

    Dev(added features): added quicksort

--- src/example.c
+++ src/example.c
@@ -6,18 +6,20 @@
 
 extern int bubblesort(int* arr, int size);
 extern int insertionsort(int* arr, int size);
+extern int quicksort(int* arr, int size);
 
 int main(int argc, char *argv[]) {
   // Create array with some integers
-  int arr[] = {11,8,2,3,4,7,5,2,1};
-  int arr2[] = {11,8,2,3,4,7,5,2,1};
-  int arr3[] = {11,8,2,3,4,7,5,2,1};
+  int arr0[] = {11,8,10,12,14,2,3,6,7,5,2,1};
+  int arr2[] = {11,8,10,12,14,2,3,6,7,5,2,1};
+  int arr3[] = {11,8,10,12,14,2,3,6,7,5,2,1};
+  int arr4[] = {11,8,10,12,14,2,3,6,7,5,2,1};
   
   /*
   *   Original
   */
   printf("\noriginal array:\n");
-  for(int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++) printf("%u, ", arr[i]);
+  for(int i = 0; i < sizeof(arr0)/sizeof(arr0[0]); i++) printf("%u, ", arr0[i]);
 
   /*
   *   Bubblesort
@@ -32,4 +34,11 @@ int main(int argc, char *argv[]) {
   insertionsort(arr3, sizeof(arr3));
   printf("\nInsertionsort:\n");
   for(int i = 0; i < sizeof(arr3)/sizeof(arr3[0]); i++) printf("%u, ", arr3[i]);
+
+  /*
+  *   Quicksort
+  */
+  quicksort(arr4, sizeof(arr4));
+  printf("\nQuicksort:\n");
+  for(int i = 0; i < sizeof(arr4)/sizeof(arr4[0]); i++) printf("%u, ", arr4[i]);
 }
